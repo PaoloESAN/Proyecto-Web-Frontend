69842bb04df15321679dab92305b986c66b433f4
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-06-06 21:03:14 -0500

    nuxt ui, pinia, tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div>
+      <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid">Button</UButton>
+      <NuxtRouteAnnouncer />
+      <NuxtWelcome />
+    </div>
+  </UApp>
 </template>
